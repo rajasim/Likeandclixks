25005a94161d18ce67dceb4654862eb842684517
Rajasim <Rajmahat@13gmail.com>
2025-02-01 12:14:27 +0530

    likeandclicks

--- src/javascript_files/Client.js
+++ src/javascript_files/Client.js
@@ -5,10 +5,15 @@ function Client() {
   return (
     <div className="App">
       <section className="brands-section">
-        <h2 className="section-title">BRANDS THAT TRUST US</h2>
+        <h2 className="section-title">More Services We Offer</h2>
         <div className="brands-container">
-          <div className="brand-card">Emcure</div>
-          <div className="brand-card">ReWaise</div>
+          <div className="brand-card">Content Strategy</div>
+          <div className="brand-card">Lead Generation</div>
+          <div className="brand-card">E-commerce</div><br>
+            
+          </br>
+          
+          
         </div>
       </section>
     </div>
